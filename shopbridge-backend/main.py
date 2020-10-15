0636b86a6f9924e3028f0d1b7978a28061a5dc77
Vinayak Rastogi <vinayak.rastogi@sokrati.com>
2020-10-15 13:10:05 +0530

    Added comments

--- shopbridge-backend/main.py
+++ shopbridge-backend/main.py
@@ -5,13 +5,16 @@ import json
 import os
 
 app = Flask(__name__, static_url_path='')
+# Handling Cross Origin Resource Sharing
 CORS(app)
 
 
+# API to list all items
 @app.route('/list', methods=['GET'])
 def list_items():
     try:
         results = []
+        # Create response JSON for each item present
         for row in DatabaseUtil().list_all_items():
             data = {
                 "id": row[0],
@@ -27,21 +30,26 @@ def list_items():
         return resp
 
 
+# API to create new item
 @app.route('/item', methods=['POST'])
 def add_item():
     try:
+        # Getting all multipart form data
         json_data = {'name': request.form['name'], 'description': request.form['description'],
                      'price': request.form['price']}
         file = request.files['imgFile']
 
+        # Calling database function to make a new entry in database
         res = DatabaseUtil().insert_item_row(json_data['name'], json_data['description'], json_data['price'])
         if res:
             data = {
                 "id": res[0][0]
             }
+            # Saving the Image file to img directory
             file.save('img/' + str(res[0][0]) + '.jpg')
             return json.dumps(data)
         else:
+            # Handling error in case database write failed with exception
             resp = jsonify({'message': 'Database Error'})
             resp.status_code = 500
             return resp
@@ -51,12 +59,14 @@ def add_item():
         return resp
 
 
+# API to get item detail using item id
 @app.route('/item/<item_id>', methods=['GET'])
 def get_item(item_id):
     try:
-        print("json_data", item_id)
+        # Calling database function to get item detail using item id
         res = DatabaseUtil().get_item_detail(item_id)
         if res:
+            # Creating response JSON
             data = {
                 "name": res[0][1],
                 "description": res[0][2],
@@ -64,6 +74,7 @@ def get_item(item_id):
             }
             return json.dumps(data)
         else:
+            # Handling the case when no data is found in database for provided item id
             resp = jsonify({'message': 'No data with associated id'})
             resp.status_code = 404
             return resp
@@ -73,30 +84,29 @@ def get_item(item_id):
         return resp
 
 
+# API to delete item with item id
 @app.route('/item/<item_id>', methods=['DELETE'])
 def delete_item(item_id):
     try:
-        if item_id:
-            DatabaseUtil().delete_item(item_id)
-            data = {
-                "message": "success"
-            }
-            file_path = 'img/'+str(item_id) + '.jpg'
-            if os.path.exists(file_path):
-                os.remove(file_path)
-            else:
-                print("The file does not exist")
-            return json.dumps(data)
+        # Calling database function to delete item with item id
+        DatabaseUtil().delete_item(item_id)
+        data = {
+            "message": "success"
+        }
+        file_path = 'img/' + str(item_id) + '.jpg'
+        # Remove image from img directory associated with item id
+        if os.path.exists(file_path):
+            os.remove(file_path)
         else:
-            resp = jsonify({'message': 'Database Error'})
-            resp.status_code = 500
-            return resp
+            print("The file does not exist")
+        return json.dumps(data)
     except Exception as e:
         resp = jsonify({'message': str(e)})
         resp.status_code = 400
         return resp
 
 
+# API to send image file from img directory
 @app.route('/img/<path:path>')
 def send_js(path):
     return send_from_directory('img', path)
